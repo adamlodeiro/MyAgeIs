b9358c4cf45a37be2ca166d8f985afb305182b4e
adamlodeiro <adam.lodeiro@patelhs.org>
2020-09-24 12:32:11 +0000

    I changed my code by adding three variable, then afterwards I checked to see f my birthday was equal to today's date and lastly if it was I had the code add 1 to my age and if not I had my age stay the same.

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 15;
+    int todaysDate = 923;
+    int myBirthDay = 731;
+
+    if (myBirthDay == todaysDate){
+      System.out.print("I am" + (myAge + 1));
+    } else {
+      System.out.print(" I am still " + myAge);
+    }
   }
 }
